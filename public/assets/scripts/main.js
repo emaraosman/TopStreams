15299c32846f704d0c807eda535ca7ceef510a9e
Osman Emara <emaraosman@gmail.com>
2017-10-28 13:20:31 -0400

    add file system and config.js

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+
+  // $("#submit-button").on("click", function(event) {
+  //
+  //   event.preventDefault();
+  //   var zipcode = $(".text-input").val();
+
+
+})
